4628b853c2258e5239d56e2f649e5828fcd69c7b
gdal@thinkpad <gdal@cs.ru.nl>
2017-04-24 09:12:42 +0200

    first release

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from setuptools import setup
 
 setup(
     name='vimgdb',
+    version='1.0.0',
     zip_safe=False,
+    description='Use Vim to visually step through source code with GNU Gdb.',
+    keywords='Vim Gdb Tmux debugger',
     license='MIT',
+    url='https://github.com/gisodal/vimgdb',
     packages=['vimgdb'],
     package_data={'vimgdb': ['config/vimrc','config/gdbinit']},
-    scripts = ['bin/vimgdb']
+    scripts = ['bin/vimgdb'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License'
+    ]
     )
