954a1a68363f1d4444ca13321b070cd14269166f
gdal@thinkpad <gdal@cs.ru.nl>
2017-04-30 22:43:12 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vimgdb',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
     description='Use Vim to visually step through source code with GNU Gdb.',
     keywords='Vim Gdb Tmux debugger',
