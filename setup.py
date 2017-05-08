9968d7abb6a5606f11405f578e37696b77774ccb
gdal@thinkpad <gdal@cs.ru.nl>
2017-05-08 21:58:12 +0200

    beta release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vimgdb',
-    version='1.0.1',
+    version='1.1.0',
     zip_safe=False,
     description='Use Vim to visually step through source code with GNU Gdb.',
     keywords='Vim Gdb Tmux debugger',
@@ -12,8 +12,8 @@ setup(
     package_data={'vimgdb': ['config/vimrc','config/gdbinit']},
     scripts = ['bin/vimgdb'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Topic :: Software Development',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Debuggers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
