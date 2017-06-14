31c0701068c8f9e98009d8ec6dec14043401f645
gdal@silveru <gdal@cs.ru.nl>
2017-06-14 18:21:07 +0200

    added feature to capture function output from vim

--- vimgdb/viminterface.py
+++ vimgdb/viminterface.py
@@ -73,6 +73,34 @@ class Vim:
         """Add command to the vimgdb batch."""
         self.command.append(command)
 
+    def CurrentFile(self):
+        return self.EvalCommand('echo expand("%:p")')
+
+    def ExecCommand(self,command):
+        cmd = [ self.executable,
+                "--servername",self.servername,
+                "--remote-send",command]
+
+        if settings.debug:
+            return subprocess.call(cmd)
+        else:
+            DEVNULL = open(os.devnull, 'w')
+            return subprocess.call(cmd,stdout=DEVNULL,stderr=subprocess.STDOUT)
+
+    def EvalCommand(self,command):
+        cmd = [ self.executable,
+                "--servername",self.servername,
+                "--remote-expr",
+                "VimgdbCommand('{0}')".format(command) ]
+
+        if settings.debug:
+            result = subprocess.check_output(cmd)
+        else:
+            DEVNULL = open(os.devnull, 'w')
+            result = subprocess.check_output(cmd,stdout=DEVNULL,stderr=subprocess.STDOUT)
+
+        return result.decode('utf-8').strip()
+
     def RunCommand(self):
         """Send all commands in the vimgdb batch."""
         if len(self.command) > 0:
@@ -103,15 +131,7 @@ class Vim:
                 function = " | ".join(function)
                 command = '<Esc>:{0}<Enter>'.format(function)
 
-            cmd = [ self.executable,
-                "--servername",self.servername,
-                "--remote-send",command]
-
-            if settings.debug:
-                return subprocess.call(cmd)
-            else:
-                DEVNULL = open(os.devnull, 'w')
-                return subprocess.call(cmd,stdout=DEVNULL,stderr=subprocess.STDOUT)
+            return self.ExecCommand(command)
         else:
             return 0
 
